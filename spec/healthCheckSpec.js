bd3e1a729e6114a046d12d3565dc03804170eafe
songawee <flapjacks999@gmail.com>
2015-02-05 21:15:12 -0700

    Separate specs into own files and update package.json

--- spec/healthCheckSpec.js
+++ spec/healthCheckSpec.js
@@ -1,4 +1,4 @@
-var server = require("../server/server.js").createServer();
+var server = require("../server/server.js").createServer(3000);
 var data = require("./fixtures/data.js");
 
 describe("Health Check", function () {
@@ -18,32 +18,3 @@ describe("Health Check", function () {
 
 });
 
-describe("get Data", function () {
-
-  it("responds with data object", function(done) {
-    var options = {
-      method: "GET",
-      url: "/data/1"
-    };
-
-    server.inject(options, function(response) {      
-      expect(response.statusCode).toBe(200);
-      expect(response.result).toEqual(data[1]);
-      done();
-    });
-  });
-
-  it("responds with 404 - Not Found", function(done) {
-    var options = {
-      method: "GET",
-      url: "/data/two"
-    };
-
-    server.inject(options, function(response) {      
-      expect(response.statusCode).toBe(404);
-      expect(response.result).toBe("Not Found");
-      done();
-    });
-  });
-
-});
